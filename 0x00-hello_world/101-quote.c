593aac9b4886039026f89f80ae5d1ac239697377
AbBiz101 <bizab093@gmail.com>
2023-02-16 21:34:04 +0100

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,14 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * *
+ * Return: 1 if Success
+ * *
  */
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char message[] = "and that piece of art is
+useful\" - Dora Korpar, 2015-10-19\n";
 write(2, message, sizeof(message));
 return (1);
 }
