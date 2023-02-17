1028802055fd11c865826c9ee47e6b36ad313863
AbBiz101 <bizab093@gmail.com>
2023-02-17 21:28:03 +0100

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
 {
-printf("%d and is less than 6 and not 0\n", n);
+printf("%d is and is less than 6 and not 0\n", n);
 }
 else if (n == 0)
 {
-printf("%d and is 0\n", n);
+printf("%d is and is 0\n", n);
 }
 else if (n < 6)
 {
-printf("%d and is less than 6 and not 0\n", n);
+printf("%d is and is less than 6 and not 0\n", n);
 }
 return (0);
 }
