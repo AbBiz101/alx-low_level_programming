13c6d7dcc66b722c0bf702ebd14e7b6c3854bd05
AbBiz101 <bizab093@gmail.com>
2023-02-17 22:54:30 +0100

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+int num;
+for (num = 0 ; num < 10 ; num++)
+{
+putchar(num + 48);
+}
+putchar('\n');
+return (0);
+}
