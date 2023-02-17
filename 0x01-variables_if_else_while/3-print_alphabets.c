b8cb2307328963b8dbe31d46d9015c6749e0b389
AbBiz101 <bizab093@gmail.com>
2023-02-17 22:13:23 +0100

    task ~

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,9 +6,14 @@
 int main(void)
 {
 char letters;
-for (letters = 'a'; letters <= 'Z'; letters++)
+for (letters = 'a'; letters <= 'z'; letters++)
 {
 putchar(letters);
 }
+for (letters = 'A'; letters <= 'Z'; letters++)
+{
+putchar(letters);
+}
+putchar('\n')
 return (0);
 }
