f3ff8fba5a7d6eff8afa2a40c22a0574bcacd001
AbBiz101 <bizab093@gmail.com>
2023-02-17 22:31:58 +0100

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,13 +3,12 @@
  * main - entry point
  * Return: 0 after printing the function
  */
-/* Task 02 */
 int main(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-while (ch != 'e' && ch != 'q')
+if (ch != 'e' && ch != 'q')
 {
 putchar(ch);
 }
