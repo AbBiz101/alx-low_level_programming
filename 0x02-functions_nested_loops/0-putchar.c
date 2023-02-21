77975e4f42ba774b927a52bc29fe611fe6c66062
AbBiz101 <bizab093@gmail.com>
2023-02-21 14:20:08 +0100

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - entry point
+* Return: 0 after printing the function
+*/
+int main(void)
+{
+char str[] = "_putchar";
+int len = strlen(str);
+int i;
+for (i = 0; i < len; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
+return (0);
+}
