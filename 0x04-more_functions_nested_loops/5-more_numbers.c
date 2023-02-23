66777d0604f15bcf4a287cf3782499aa01a5f9c4
AbBiz101 <bizab093@gmail.com>
2023-02-23 22:52:55 +0100

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,24 +5,23 @@
  * @void: it does not take any number
  * Return: no return
  */
-
 void more_numbers(void)
 {
-int j, k;
-int i = 10;
-while (i < 10)
+int i, k;
+int j = 0;
+while (j < 10)
 {
-for (j = 0; j <= 14; j++)
+for (i = 0; i <= 14; i++)
 {
-k = j;
+k = i;
 if (k > 9)
 {
 _putchar('1');
-k = j % 10;
+k = i % 10;
 }
 _putchar('0' + k);
 }
 _putchar('\n');
-i++;
+j++;
 }
 }
