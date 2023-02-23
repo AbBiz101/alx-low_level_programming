0c258073feba179e928ad8bf3a1b910856da83b3
AbBiz101 <bizab093@gmail.com>
2023-02-24 00:25:42 +0100

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz test
+ * Return: 0
+ */
+int main(void)
+{
+int j;
+for (j = 1; j <= 100; j++)
+{
+if (j % 3 == 0 && j % 5 == 0)
+{
+printf("FizzBuzz\n");
+}
+else if (j % 3 == 0)
+{
+printf("Fizz\n");
+}
+else if (j % 5 == 0)
+{
+printf("Buzz\n");
+}
+else
+{
+printf("%d\n", j);
+}
+}
+return (0);
+}
