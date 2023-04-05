385cd5aff120b09bf245c1e3653d1edd3fced5a0
AbBiz101 <bizab093@gmail.com>
2023-04-05 17:10:05 +0200

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,39 +5,21 @@
  * @dest: a pointer to a string
  * @src: a pointer to a sting
  * Return: returns a pointer to the resulting string dest
- */
+ **/
 char *_strcat(char *dest, char *src)
 {
-int i = 0, j = 0, k = 0;
-int sizedest;
-int sizesrc;
-int totalsize;
-while (i >= 0)
+int i = 0;
+int j = 0;
+while (dest[i] != '\0')
 {
-if (dest[i] == '\0')
-{
-sizedest = i;
-break;
-}
 i++;
 }
-i = 0;
-while (i >= 0)
+while (src[j] != '\0')
 {
-if (src[i] == '\0')
-{
-sizesrc = i;
-break;
-}
+dest[i] = src[j];
 i++;
-}
-totalsize = sizedest + sizesrc;
-j = sizedest;
-while (j < totalsize)
-{
-dest[j] = src[k];
 j++;
-k++;
 }
+dest[i] = '\0';
 return (dest);
 }
