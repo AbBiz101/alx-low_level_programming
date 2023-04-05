adea19aea3303231689200ab65b2ddc15135e79a
AbBiz101 <bizab093@gmail.com>
2023-04-06 00:32:16 +0200

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,21 +7,16 @@
  */
 char *rot13(char *s)
 {
-char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-int i, j;
-for (i = 0; s[i] != '\0'; i++)
-{
-j = 0;
-while (input[j] != '\0')
-{
-if (s[i] == input[j])
-{
-s[i] = output[j];
-break;
+int i;
+char c;
+for (i = 0; s[i] != '\0'; i++) {
+c = s[i];
+if (c >= 'a' && c <= 'z') {
+c = ((c - 'a' + 13) % 26) + 'a';
+} else if (c >= 'A' && c <= 'Z') {
+c = ((c - 'A' + 13) % 26) + 'A';
 }
-j++;
+s[i] = c;
 }
-}
-return (s);
+return s;
 }
