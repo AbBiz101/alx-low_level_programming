5394359891c48c9506015ea2e1d188ead4c323f4
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2023-04-07 00:51:39 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-
-	if (b == NULL)
-		return (0);
-
-	while (*b)
-	{
-		if (*b == '0' || *b == '1')
-			num = (num << 1) | (*b - '0');
-		else
-			return (0);
-		b++;
-	}
-
-	return (num);
+unsigned int num = 0;
+if (b == NULL)
+	return (0);
+while (*b)
+{
+if (*b == '0' || *b == '1')
+	num = (num << 1) | (*b - '0');
+else
+	return (0);
+b++;
+}
+return (num);
 }
