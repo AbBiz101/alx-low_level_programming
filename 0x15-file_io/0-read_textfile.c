d1ed974127f519a7791335d94c3bd5d3cd00f2e4
AbBiz101 <bizab093@gmail.com>
2023-07-10 11:14:21 -0500

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,39 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints
- *                 it to the POSIX standard output
- * @filename: a pointer to to a string of 0 and 1 chars
- * @letters: is the number of letters it should read and print
- * Return: the actual number of letters it could read and print
- *         and returns 0 if the file can not be opened or read,
- *         if filename is NULL, f write fails or does
- *         not write the expected amount of bytes
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-FILE *file;
-char *buffer;
-ssize_t num_read, num_written;
-if (filename == NULL)
-	return (0);
-file = fopen(filename, "r");
-if (file == NULL)
-	return (0);
-buffer = malloc((letters + 1) * sizeof(char));
-if (buffer == NULL)
-{
-fclose(file);
-return (0);
-}
-num_read = fread(buffer, sizeof(char), letters, file);
-if (ferror(file) != 0 || num_read == 0)
-{
-free(buffer);
-fclose(file);
-return (0);
-}
-buffer[num_read] = '\0';
-num_written = printf("%s", buffer);
-free(buffer);
-fclose(file);
-if (num_written != num_read)
-	return (0);
-return (num_read);
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
 }
